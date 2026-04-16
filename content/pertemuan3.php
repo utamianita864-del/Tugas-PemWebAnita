0701446b705e5d4b60809ce19f6d269be6cd06b5
utamianita864-del <utamianita864@gmail.com>
2026-04-16 20:44:04 +0700

    Add files via upload

---
+++ content/pertemuan3.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initialscale=1.0">
+        <title>Informasi Mata Kuliah Pemrograman Web</title>
+</head>
+<body>
+
+<!-- Heading -->
+        <h1>Pertemuan minggu pertama dari mata kuliah pemrograman
+web (H1)</h1>
+        <h2>Pertemuan minggu pertama dari mata kuliah pemrograman 
+web (H2)</h2>
+        <h3>Pertemuan minggu pertama dari mata kuliah pemrograman 
+web (H3)</h3>
+        <h4>Pertemuan minggu pertama dari mata kuliah pemrograman 
+web (H4)</h4>
+        <h5>Pertemuan minggu pertama dari mata kuliah pemrograman 
+web (H5)</h5>
+        <hr>
+        <!-- Paragraf -->
+        ini teks biasa
+        <p>ini adalah teks paragraf</p>
+        <b>ini adalah teks paragraf dengan bold</b><br>
+        <strong>ini adalah teks paragraf dengan strong</strong><br>
+        <i>ini adalah teks paragraf dengan italic</i><br>
+        <code>ini adalah teks paragraf dengan code</code><br>
+        <!-- link -->
+        ini adalah teks untuk link <a
+href="https://www.instagram.com">Link Instagram</a>
+        <hr>
+        <!-- list -->
+        <!-- ol untuk list dengan angka -->
+        <ol>
+           <li>satu</li>
+           <li>dua</li>
+           <li>tiga</li>
+        </ol>
+        <!-- ul untuk list dengan simbol -->
+        <ul>
+           <li>satu</li>
+           <li>dua</li>
+           <li>tiga</li>
+        </ul>
+        <hr>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/250px-Instagram_logo_2016.svg.png" alt="ig" ,width="100" , height="100">
+        <img src="https://img.freepik.com/vektor-premium/logo-facebook-vektor-logo-resmi-facebook-vektor-illustrator-logo-facebook_1002350-1803.jpg" alt="ig" , width="100" , height="100">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/960px-WhatsApp.svg.png" alt="ig" , width="100" ,height="100">
+        <img src="https://images.icon-icons.com/3685/PNG/512/github_logo_icon_229278.png" alt="ig" ,width="100" , height="100">
+</body>
+</html>
---
+++ content/pertemuuan4.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 4</title>
+</head>
+<body>
+    <h3>Praktikum Pertemuan 4</h3>
+    
+    <!-- Tabel Data Mahasiswa -->
+    <table border="1">
+        <tr>
+            <th colspan="5">Data Mahasiswa</th>   
+        </tr>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Umur</th>
+            <th>Foto</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Rizly</td>
+            <td>Jl. Merdeka No. 123</td>
+            <td>20</td>
+            <td><img src="https://cdn.idntimes.com/content-images/post/20230517/rizkyridhoramadhani-347076200-556022146688735-9107025040918080457-n-6a30a7a53b62191b4473e5acd258224d_600x400.jpg" alt="Foto Rizly" width="100"></td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Siti</td>
+            <td>Jl. Sudirman No. 456</td>
+            <td>20</td>
+            <td><img src="https://img.okezone.com/content/2023/08/17/43/2866156/fakta-wilda-siti-nurfadhilah-mundur-dari-timnas-voli-putri-indonesia-9Fczs1SWjj.jpg" alt="Foto Siti" width="100"></td>
+        </tr>
+    </table>
+
+    <!-- Tabel Daftar Smartphone -->
+    <br><br>
+    <table border="1">
+        <tr>
+            <th colspan="5">Daftar Smartphone</th>
+        </tr>
+        <tr>
+            <th>No</th>
+            <th>Nama Brand</th>
+            <th>Gambar</th>
+            <th>Keterangan</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Apple</td>
+            <td align="center">
+                <img src="https://asset-2.tstatic.net/jogja/foto/bank/images/Daftar-Harga-HP-iPhone-14-Pro-Terbaru-November-2022.jpg" width="100" height="100" alt="iPhone">
+            </td>
+            <td>
+                iPhone 16 Pro Max<br>
+                Dengan desain titanium yang memukau. Kontrol kamera. Dolby Vision 4K 120 fps. Dan chip A18 Pro. 
+                Rp 20.000.000
+            </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Samsung</td>
+            <td align="center">
+                <img src="https://tse3.mm.bing.net/th/id/OIP.J4wSFz4LpttbE4yzixRrYgHaDd?pid=Api&h=220&P=0" width="100" height="100" alt="Samsung Galaxy">
+            </td>
+            <td>
+                Samsung Galaxy S24 Ultra<br>
+                HP premium dengan layar besar, S Pen, kamera canggih, dan performa sangat mewah. 
+                Rp 18.000.000
+            </td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Xiaomi</td>
+            <td align="center">
+                <img src="https://tse3.mm.bing.net/th/id/OIP.d5UaI2rl43bjHHMxW-5b6AHaHa?pid=Api&h=220&P=0" width="100" height="100" alt="Xiaomi 12">
+            </td>
+            <td>
+                Xiaomi 12 8/256<br>
+                HP flagship dengan performa kencang, desain elegan, dan harga lebih terjangkau. 
+                Rp 8.000.000
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
